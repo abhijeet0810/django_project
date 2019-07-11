4bcf26611a11704129c6c7ba536cd25edd2414ff
abti0810@gmail.com <abti0810@gmail.com>
2019-07-11 17:22:19 +0200

    added docker

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,12 +78,30 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'startup_companies',
+        'USER': 'root',
+        'PASSWORD': 'Abhijeet@88',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+
 
 
 # Password validation
